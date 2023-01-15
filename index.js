61a9bdec25ee4c553562b211c42ba8fda792696d
asadi18 <asadishteaque@gmail.com>
2023-01-15 13:24:33 +0600

    Real time Character Counter using javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+const textareaEl = document.getElementById("textarea")
+const totalcounterEl = document.getElementById("total-counter")
+const remainingCounterEl = document.getElementById("remaining-counter")
+
+textareaEl.addEventListener("keyup", ()=>{
+    updateCounter()
+})
+updateCounter()
+
+function updateCounter(){
+    totalcounterEl.innerText = textareaEl.value.length;
+    remainingCounterEl.innerText = textareaEl.getAttribute("maxLength") - textareaEl.value.length;
+}
